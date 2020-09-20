538207a00fd9ddfa1ca990306156a96184b66713
mhdnashalghifarl <mhdnasrullahal@gmail.com>
2020-09-20 21:55:18 +0700

    Evaluasi

---
+++ Evaluasi-2/latihan-2.1.php
@@ -0,0 +1,32 @@
+<?php
+
+class PendaftaranSantriGontor{
+   protected $dataSantri = [
+      [
+         'User ID'=>'M-10',
+         'Nama'=>'Fadil'
+      ],
+      [
+         'User ID'=>'M-11',
+         'Nama'=>'Fahri'
+      ],
+   ];
+
+   public function tambahdatasantri(){
+      $i=count(this->dataSantri);
+      echo "Input ID Santri = ";
+      $this->dataSantri[$i]['User ID']=trim(fgets(STDIN));
+      echo "Input Nama Santri = ";
+      $this->dataSantri[$i]['Nama']=trim(fgets(STDIN));
+      print_r ($this->dataSantri);
+
+   }
+}
+
+class PendaftaranSantriBaru extends PendaftaranSantriGontor{
+      public $daftar_santri = [
+         'Nama'   =>'Rahmat'
+      ]
+}
+
+
