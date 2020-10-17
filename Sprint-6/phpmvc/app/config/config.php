992151a7bbf0d630ae936bbb495a2dad35d6a0b3
mhdnashalghifarl <mhdnasrullahal@gmail.com>
2020-10-17 08:57:53 +0700

    Sprint-6

---
+++ Sprint-6/phpmvc/app/config/config.php
@@ -0,0 +1,9 @@
+<?php
+
+define('BASEURL', 'http://localhost/Kurikulum-Backend/Sprint-6/phpmvc/public');
+
+// DB
+define('DB_HOST', 'localhost');
+define('DB_USER', 'user');
+define('DB_PASS', 'password');
+define('DB_NAME', 'phpmvc');
---
+++ Sprint-6/phpmvc/app/views/about/index.php
@@ -0,0 +1,11 @@
+<div class="container">
+    <div class="card" style="width: 18rem;">
+        <img src="<?= BASEURL; ?>/img/bunga.jpeg" class="card-img-top" alt="Muhammad" width="200">
+        <div class="card-body">
+            <h4 class="card-title">About Me</h4>
+            <p>Halo, nama saya <?= $data['nama']; ?>, umur saya <?= $data['umur']; ?> tahun, saya adalah seorang <?= $data['pekerjaan']; ?>.</p>
+            <a href="<?= BASEURL; ?>" class="btn btn-outline-secondary">Back</a>
+        </div>
+    </div>
+
+</div>
